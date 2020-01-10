ee64b1275499f8e24c1394ffa22a9f7d73845ffa
Cpapa97 <cpapa97@gmail.com>
2020-01-10 16:22:39 -0600

    move requirements back to a separate file

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
+with open(path.join(here, 'requirements.txt')) as f:
+    requirements = f.read().split()
+
 setup(
     name='minnie-config',
     version='0.0.1',
@@ -12,5 +15,5 @@ setup(
     author='Christos Papadopoulos',
     author_email='cpapadop@bcm.edu',
     packages=find_packages(exclude=[]),
-    install_requires=['datajoint', 'numpy', 'h5py']
+    install_requires=requirements
 )
