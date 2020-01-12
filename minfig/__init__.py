fd7517bde3d9ab7af027401e05b8f4aae20c84e5
Cpapa97 <cpapa97@gmail.com>
2020-01-12 17:32:10 -0600

    add universal configure method that can return an automatically made virtual module

--- minfig/__init__.py
+++ minfig/__init__.py
@@ -6,3 +6,12 @@ from .minnie65_config import *
 from .adapters import mesh, decimated_mesh, adapter_objects
 
 # Just need to run verify_paths() and set_configurations()
+# Or use this...
+
+def configure(return_virtual_module=False, create_if_missing=False):
+    verify_paths(create_if_missing=create_if_missing)
+    set_configurations()
+
+    if return_virtual_module:
+        import datajoint as dj
+        return dj.create_virtual_module('minnie', schema_name_m65, add_objects=adapter_objects)
